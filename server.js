fcb0b1d5b37b027f35f9fa3e5e4420d35938796b
Jeremiah <jeremy19ketema@gmail.com>
2026-04-25 22:10:20 +0300

    Fix 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
+const path = require('path');
 
  const server = http.createServer((req, res) => {
     if (req.url === '/api/clinics' && req.method === 'GET') {
